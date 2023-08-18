4bdf4c1fdfb3456f4f596fecf63a16eb2d5d7144
Cinerarius <koustavrakshit10b@gmail.com>
2023-08-18 18:09:22 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'assets', 
+    assetsDir: "assets",
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/path/to/main.js",
+    },
   },
-})
+});
